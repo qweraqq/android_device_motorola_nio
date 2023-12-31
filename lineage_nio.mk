edbafaae12ab147bd533837b92d15d513e45b1a5
shenxx <shenxiangxiang@gmail.com>
2023-12-31 14:34:37 +0800

    fix typo

--- lineage_nio.mk
+++ lineage_nio.mk
@@ -26,5 +26,5 @@ PRODUCT_SYSTEM_NAME := nio_retcn
 # Build info
 BUILD_FINGERPRINT := "motorola/nio_retcn/nio:13/S1RN32M.55-16-13/b9f4e9:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="nio_retcn-user 13 S1RN32M.55-16-13b9f4e9 release-keys" \
+    PRIVATE_BUILD_DESC="nio_retcn-user 13 S1RN32M.55-16-13 b9f4e9 release-keys" \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
